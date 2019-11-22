a679c6889445896b1690da7f918fea167469ad5c
Matheus Paice <matheuspaice@mandeumzap.com.br>
2019-11-22 15:00:16 -0300

    config devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducers from "./ducks";
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, composeWithDevTools());
 
 export default store;
